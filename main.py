e85f4583217a841189ecb51ce9be9f00d8f5d071
muder123 <wycgjmh@gmail.com>
2021-05-27 22:11:55 +1000

    paddles created, ball moving

---
+++ main.py
@@ -0,0 +1,28 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+import time
+
+screen = Screen()
+screen.bgcolor('black')
+screen.setup(width=800, height=600)
+screen.title('Pong')
+screen.tracer(0)
+
+l_paddle = Paddle((-350,0))
+r_paddle = Paddle((350,0))
+ball = Ball()
+
+screen.listen()
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, 's')
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, 'Down')
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+screen.exitonclick()
